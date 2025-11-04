cb59fae9dd933daf2c4f902713f141c98fe0d7cd
FrancoSaracho64 <fmsaracho64@gmail.com>
2025-11-03 22:02:58 -0300

    Se solicitan los datos al cliente

--- src/main/java/ar/edu/unlu/bdd/clientes/Cliente.java
+++ src/main/java/ar/edu/unlu/bdd/clientes/Cliente.java
@@ -1,7 +1,41 @@
 package ar.edu.unlu.bdd.clientes;
 
+import ar.edu.unlu.bdd.helper.Constants;
+import ar.edu.unlu.bdd.utils.Menu;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Cliente {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        int puertoSwitch = Constants.PORT_SERVER_SWITCH;
+        Menu menu = new Menu();
+        menu.solicitarEntradas();
+
+        String tabla = menu.getTabla();
+        String query = menu.getQuery();
+
+        String xml = """
+                <query>
+                    <database>%s</database>
+                    <sql>%s</sql>
+                </query>
+                """.formatted(tabla, query);
+
+        System.out.println("Enviando al switch:\n" + xml);
+        try (var socket = new Socket("localhost", puertoSwitch);
+             var out = new PrintWriter(socket.getOutputStream(), true);
+             var in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            out.println(xml);
+            out.flush();
+            socket.shutdownOutput();
+
+            String respuesta = in.lines().reduce("", (a, b) -> a + b + "\n");
+            System.out.println("Respuesta recibida:\n" + respuesta);
+        }
     }
 }
--- src/main/java/ar/edu/unlu/bdd/helper/Constants.java
+++ src/main/java/ar/edu/unlu/bdd/helper/Constants.java
@@ -4,8 +4,12 @@ public class Constants {
     private Constants() {
     }
 
+    // PUERTOS
     public static final Integer PORT_SERVER_SWITCH = 5000;
     public static final Integer PORT_SERVER_POSTGRESQL = 5001;
     public static final Integer PORT_SERVER_FIREBIRD = 5002;
 
+    // TABLAS
+    public static final String TABLE_PERSONAL = "PERSONAL";
+    public static final String TABLE_FACTURACION = "FACTURACION";
 }
