ef4f0fdcc2494450f739cac5f0dddb9b8d5b4faa
gustavo <lito.contardi@hotmail.com>
2025-11-03 23:32:37 -0300

    commit

--- src/main/java/ar/edu/unlu/bdd/servidores/ServidorPostgreSQL.java
+++ src/main/java/ar/edu/unlu/bdd/servidores/ServidorPostgreSQL.java
@@ -1,7 +1,29 @@
 package ar.edu.unlu.bdd.servidores;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ServidorPostgreSQL {
+
+    // PostgreSQL (PERSONAL) -> 192.168.3.4:5001
+
+    public static Connection conectar() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/personal", "postgres", "1967");
+            System.out.println("✅ Conexión exitosa a PostgreSQL.");
+        } catch (SQLException e) {
+            System.out.println("❌ Error conectando a la base de datos.");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ServidorPostgreSQL.conectar();
     }
 }
